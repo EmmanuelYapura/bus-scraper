598cb2298e5f0b4757921ed4adb56cf8d0cc0c61
Emmanuel Yapura <eyapura96@gmail.com>
2025-08-25 23:33:43 -0300

    agrega archivos constantes

---
+++ app/constantes/constantes.py
@@ -0,0 +1,23 @@
+HEADERS = {
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'accept-language': 'es-AR,es;q=0.7',
+    'cache-control': 'no-cache',
+    'origin': 'https://www.devivobus.com',
+    'pragma': 'no-cache',
+    'priority': 'u=1, i',
+    'referer': 'https://www.devivobus.com/',
+    'sec-ch-ua': '"Not;A=Brand";v="99", "Brave";v="139", "Chromium";v="139"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'sec-gpc': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
+}
+
+URL = 'https://www.devivobus.com/inventory/'
+
+API_URL = 'https://api.soarr.com/inventory/1.0/'
+
+APIKEY = '177f561a'
